dc6f98cd901addc6c97beb89f474d77b689c8d3b
Mihail Orehov <mihaiorehov@yandex.ru>
2021-03-25 22:24:31 +0300

    Исправлен профайл и popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,43 @@
 //создать переменную всплывающего окна
 const popup = document.querySelector('.popup');
 //создать переменную открытия окна для кнопки button
-const openPopupButton = document.getElementById('openPopup_button');
+const openPopupButton = document.querySelector('.profile__edit-button');
 //создать переменную закрытия окна popup
 const closePopupButton = document.querySelector('.popup__close');
 //переменная для закрытия окна popup по клику на внешнюю область вокруг 
-const popupOverlay = document.querySelector('.popup__overlay');
-
-let saveButton = document.querySelector('.form__btn-save');
-
-//функция добавления класса открытия popup
-function openPopup() {
-  popup.classList.add('popup__visible');
-}
-
-//функция удаления класса для закрытия popup
-function closePopup() {
-  popup.classList.remove('popup__visible');
-}
-//обработчик события открыть popup
-openPopupButton.addEventListener('click', function() { openPopup(); });
-//обработчик события закрыть popup для крестика
-closePopupButton.addEventListener('click', function() { closePopup(); });
-//обработчик события закрыть popup для области overlay
-popupOverlay.addEventListener('click', function() { closePopup(); });
-
+const popupOverlay = document.querySelector('.popup_overlay');
 // Находим форму в DOM
 let formElement = document.querySelector('.form');// Воспользуйтесь методом querySelector()
 // Находим поля формы в DOM
-let nameInput = formElement.querySelector('.form__input-name');// Воспользуйтесь инструментом .querySelector()
-let jobInput = formElement.querySelector('.form__input-job');// Воспользуйтесь инструментом .querySelector()
+let nameInput = formElement.querySelector('.popup__input_name_text');// Воспользуйтесь инструментом .querySelector()
+let jobInput = formElement.querySelector('.popup__input_job_text');// Воспользуйтесь инструментом .querySelector()
+// Выберите элементы, куда должны быть вставлены значения полей
+let paragraphNam = document.querySelector('.profile__paragraph-nam');
+let paragraphJob = document.querySelector('.profile__paragraph-job');
+
+
+//функция добавления класса открытия popup
+function openPopup() {
+  popup.classList.add('popup_visible');
+
+  let avatarNam = paragraphNam.value;
+  let avatarJob = paragraphJob.value;
+  
+  avatarNam = nameInput.textContent;
+  avatarJob = jobInput.textContent;
+}
+
+formElement.addEventListener('submit', openPopup); 
+
+//функция удаления класса для закрытия popup
+function closePopup() {
+  popup.classList.remove('popup_visible');
+}
+
+//обработчик события открыть popup
+openPopupButton.addEventListener('click', openPopup);
+//обработчик события закрыть popup для крестика
+closePopupButton.addEventListener('click', closePopup);
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -41,16 +49,13 @@ function formSubmitHandler (evt) {
     // Получите значение полей jobInput и nameInput из свойства value
     let nam = nameInput.value;
     let job = jobInput.value;
-    // Выберите элементы, куда должны быть вставлены значения полей
-    let paragraphNam = document.querySelector('.profile__paragraph-nam');
-    let paragraphJob = document.querySelector('.profile__paragraph-job');
     // Вставьте новые значения с помощью textContent
     paragraphNam.textContent = nam;
     paragraphJob.textContent = job;
+
+    closePopup();
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-saveButton.addEventListener('click', function() { closePopup(); });
-formElement.addEventListener('click', formSubmitHandler);
 formElement.addEventListener('submit', formSubmitHandler); 
